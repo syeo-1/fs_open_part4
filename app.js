c57068ca50f974e61c7f6a26a7b9a2942d4f10bd
Sean Yeo <seansqyeo@gmail.com>
2020-07-23 22:52:00 -0700

    configure app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs') 
+
+logger.info('connecting to', config.MONGODB_URI)
+// const blogSchema = mongoose.Schema({
+//   title: String,
+//   author: String,
+//   url: String,
+//   likes: Number
+// })
+
+// const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
