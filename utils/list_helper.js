f23df12a5323149869a6e6a5c83cec308628d3f4
Sean Yeo <seansqyeo@gmail.com>
2020-07-25 14:34:28 -0700

    finish 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,8 +1,19 @@
 const dummy = (blogs) => {
-  console.log(blogs)
   return 1
+}
+
+const totalLikes = (blogs) => {
+  const reducer = (accumulator, current) => {
+    console.log(accumulator)
+    return accumulator + current.likes
+  }
+
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer, 0)
 }
 
 module.exports = {
   dummy,
+  totalLikes,
 }
