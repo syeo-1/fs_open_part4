41ddb403c78eca8a8fa1f1fdc53283ddb871843c
Sean Yeo <seansqyeo@gmail.com>
2020-07-26 23:24:09 -0700

    finish 4.10

--- tests/bloglist_api.test.js
+++ tests/bloglist_api.test.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const helper = require('./test_helper')
+const { initialNotes } = require('./test_helper')
 
 const api = supertest(app)
 
@@ -14,6 +16,24 @@ test('there are two blogs', async() => {
   const response = await api.get('/api/blogs')
 
   expect(response.body).toHaveLength(2)
+})
+test('a blog is added to the blog list', async() => {
+  const newBlog = {
+    title: 'testblog',
+    author: 'me',
+    url: 'http://localhost:3003/api/blogs',
+    likes: 2,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(initialNotes.length+1)
 })
 
 afterAll(() => {
---
+++ tests/test_helper.js
@@ -0,0 +1,22 @@
+// const Note = require('../models/blog')
+
+const initialNotes = [
+  {
+    'title': 'Slush: Probably the best conference in the world',
+    'author': 'FundingBox',
+    'url': 'https://medium.com/fundingbox-blog/slush-probably-the-best-conference-in-the-world-42329215c612',
+    'likes': 10,
+    'id': '5f1e583d0d3fc32d98d659c3'
+  },
+  {
+    'title': 'Slush: Probably the best conference in the world',
+    'author': 'FundingBox',
+    'url': 'https://medium.com/fundingbox-blog/slush-probably-the-best-conference-in-the-world-42329215c612',
+    'likes': 10,
+    'id': '5f1e58480d3fc32d98d659c4'
+  }
+]
+
+module.exports = {
+  initialNotes
+}
