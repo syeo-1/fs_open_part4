ff6ee43e2400938d49aeabda4e5afc66a00a16db
Sean Yeo <seansqyeo@gmail.com>
2020-08-02 22:21:16 -0700

    finish 4.15

--- app.js
+++ app.js
@@ -5,15 +5,21 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs') 
-
-logger.info('connecting to', config.MONGODB_URI)
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
 
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+
 
 module.exports = app
